f06f362871addb68322a6a6e258435447581fe1b
Roque Pinel <repinel@gmail.com>
2011-10-29 16:44:48 +0000

    Adicionando tabela de resultados

---
+++ src/br/ufrj/coppe/pinel/ligeiro/data/Result.java
@@ -0,0 +1,130 @@
+package br.ufrj.coppe.pinel.ligeiro.data;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ * @author Roque Pinel
+ *
+ */
+public class Result
+{
+	private String element;
+
+	private int det;
+
+	private int ret_ftr;
+
+	private String complexity;
+
+	private int complexityValue;
+
+	private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+	/**
+	 * Default contructor.
+	 */
+	public Result()
+	{
+		// empty
+	}
+
+	public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener)
+	{
+		propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener listener)
+	{
+		propertyChangeSupport.removePropertyChangeListener(listener);
+	}
+
+	/**
+	 * @return the element
+	 */
+	public String getElement()
+	{
+		return element;
+	}
+
+	/**
+	 * @param element the element to set
+	 */
+	public void setElement(String element)
+	{
+		propertyChangeSupport.firePropertyChange("element", this.element, this.element = element);
+	}
+
+	/**
+	 * @return the det
+	 */
+	public int getDet()
+	{
+		return det;
+	}
+
+	/**
+	 * @param det the det to set
+	 */
+	public void setDet(int det)
+	{
+		propertyChangeSupport.firePropertyChange("det", this.det, this.det = det);
+	}
+
+	/**
+	 * @return the ret_ftr
+	 */
+	public int getRet_ftr()
+	{
+		return ret_ftr;
+	}
+
+	/**
+	 * @param ret_ftr the ret_ftr to set
+	 */
+	public void setRet_ftr(int ret_ftr)
+	{
+		propertyChangeSupport.firePropertyChange("ret_ftr", this.ret_ftr, this.ret_ftr = ret_ftr);
+	}
+
+	/**
+	 * @return the complexity
+	 */
+	public String getComplexity()
+	{
+		return complexity;
+	}
+
+	/**
+	 * @param complexity the complexity to set
+	 */
+	public void setComplexity(String complexity)
+	{
+		propertyChangeSupport.firePropertyChange("complexity", this.complexity, this.complexity = complexity);
+	}
+
+	/**
+	 * @return the complexityValue
+	 */
+	public int getComplexityValue()
+	{
+		return complexityValue;
+	}
+
+	/**
+	 * @param complexityValue the complexityValue to set
+	 */
+	public void setComplexityValue(int complexityValue)
+	{
+		propertyChangeSupport.firePropertyChange("complexityValue", this.complexityValue, this.complexityValue = complexityValue);
+	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString()
+	{
+		return element;
+	}
+}
--- src/br/ufrj/coppe/pinel/ligeiro/views/LigeiroView.java
+++ src/br/ufrj/coppe/pinel/ligeiro/views/LigeiroView.java
@@ -1,37 +1,64 @@
 package br.ufrj.coppe.pinel.ligeiro.views;
 
+import java.util.ArrayList;
+import java.util.List;
 
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.part.*;
-import org.eclipse.jface.viewers.*;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.jface.action.*;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.*;
-import org.eclipse.swt.widgets.Menu;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.dialogs.ViewContentProvider;
+import org.eclipse.ui.part.ViewPart;
 
+import br.ufrj.coppe.pinel.ligeiro.data.Result;
 
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
+ * This sample class demonstrates how to plug-in a new workbench view. The view
+ * shows data obtained from the model. The sample creates a dummy model on the
+ * fly, but a real implementation would connect to the model available either in
+ * this or another plug-in (e.g. the workspace). The view is connected to the
+ * model using a content provider.
  * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
+ * The view uses a label provider to define how model objects should be
+ * presented in the view. Each view can present the same model objects using
+ * different labels and icons, if needed. Alternatively, a single label provider
+ * can be shared between views in order to ensure that objects of the same type
+ * are presented in the same way everywhere.
  * <p>
  */
 
-public class LigeiroView extends ViewPart {
-
+public class LigeiroView extends ViewPart
+{
 	/**
 	 * The ID of the view as specified by the extension.
 	 */
@@ -42,70 +69,162 @@ public class LigeiroView extends ViewPart {
 	private Action action2;
 	private Action doubleClickAction;
 
-	/*
-	 * The content provider class is responsible for
-	 * providing objects to the view. It can wrap
-	 * existing objects in adapters or simply return
-	 * objects as-is. These objects may be sensitive
-	 * to the current input of the view, or ignore
-	 * it and always show the same content 
-	 * (like Task List, for example).
-	 */
-	 
-	class ViewContentProvider implements IStructuredContentProvider {
-		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
-		}
-		public void dispose() {
-		}
-		public Object[] getElements(Object parent) {
-			return new String[] { "One", "Two", "Three" };
-		}
-	}
-	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
-		public String getColumnText(Object obj, int index) {
-			return getText(obj);
-		}
-		public Image getColumnImage(Object obj, int index) {
-			return getImage(obj);
-		}
-		public Image getImage(Object obj) {
-			return PlatformUI.getWorkbench().
-					getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
-		}
-	}
-	class NameSorter extends ViewerSorter {
-	}
-
 	/**
 	 * The constructor.
 	 */
-	public LigeiroView() {
+	public LigeiroView()
+	{
+		super();
 	}
 
 	/**
-	 * This is a callback that will allow us
-	 * to create the viewer and initialize it.
+	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
 	 */
-	public void createPartControl(Composite parent) {
-		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		viewer.setContentProvider(new ViewContentProvider());
-		viewer.setLabelProvider(new ViewLabelProvider());
-		viewer.setSorter(new NameSorter());
-		viewer.setInput(getViewSite());
+	public void createPartControl(Composite parent)
+	{
+		GridLayout layout = new GridLayout(2, false);
+		parent.setLayout(layout);
 
-		// Create the help context id for the viewer's control
-		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(), "LigeiroPlug-in.viewer");
-		makeActions();
-		hookContextMenu();
-		hookDoubleClickAction();
-		contributeToActionBars();
+		Label viewLabel = new Label(parent, SWT.NONE);
+		viewLabel.setText("Ligeiro Plug-in");
+		Label emptyLabel = new Label(parent, SWT.NONE);
+		emptyLabel.setText("");
+
+		Label configurationLabel = new Label(parent, SWT.NONE);
+		configurationLabel.setText("Configuration: ");
+		configurationLabel.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL));
+
+		Text searchText = new Text(parent, SWT.BORDER | SWT.SEARCH);
+		searchText.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL));
+
+		createTableViewer(parent);
+//
+//		// Create the help context id for the viewer's control
+//		PlatformUI.getWorkbench().getHelpSystem()
+//				.setHelp(viewer.getControl(), "LigeiroPlug-in.viewer");
+//		makeActions();
+//		hookContextMenu();
+//		hookDoubleClickAction();
+//		contributeToActionBars();
 	}
 
-	private void hookContextMenu() {
+	private void createTableViewer(Composite parent)
+	{
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
+		createColumns(parent, viewer);
+		final Table table = viewer.getTable();
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+
+		viewer.setContentProvider(new ArrayContentProvider());
+
+		// Get the content for the viewer, setInput will call getElements in the contentProvider
+		viewer.setInput(ModelProvider.INSTANCE.getResults());
+		//viewer.setInput(getViewSite());
+
+		// Make the selection available to other views
+		getSite().setSelectionProvider(viewer);
+	}
+
+	// This will create the columns for the table
+	private void createColumns(final Composite parent, final TableViewer viewer)
+	{
+		String[] titles = {"Element", "RET/FTR", "DET", "Complexity", "Value"};
+		int[] bounds = {100, 100, 100, 100, 100};
+
+		// First column is for the element
+		TableViewerColumn col = createTableViewerColumn(titles[0], bounds[0], 0);
+		col.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				Result p = (Result) element;
+				return p.getElement();
+			}
+		});
+
+		// Second column is for the det
+		col = createTableViewerColumn(titles[1], bounds[1], 0);
+		col.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				Result p = (Result) element;
+				return Integer.toString(p.getDet());
+			}
+		});
+
+		// Third column is for the ret/ftr
+		col = createTableViewerColumn(titles[2], bounds[2], 0);
+		col.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				Result p = (Result) element;
+				return Integer.toString(p.getRet_ftr());
+			}
+		});
+
+		// Fourth column is for the complexity
+		col = createTableViewerColumn(titles[3], bounds[3], 0);
+		col.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				Result p = (Result) element;
+				return p.getComplexity();
+			}
+		});
+
+		// Fifth column is for the complexity value
+		col = createTableViewerColumn(titles[3], bounds[3], 0);
+		col.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				Result p = (Result) element;
+				return Integer.toString(p.getComplexityValue());
+			}
+		});
+	}
+
+	private TableViewerColumn createTableViewerColumn(String title, int bound, final int colNumber)
+	{
+		final TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE);
+		final TableColumn column = viewerColumn.getColumn();
+		column.setText(title);
+		column.setWidth(bound);
+		column.setResizable(true);
+		column.setMoveable(true);
+		return viewerColumn;
+	}
+
+	private enum ModelProvider {
+		INSTANCE;
+
+		private List<Result> results;
+
+		private ModelProvider() {
+			results = new ArrayList<Result>();
+
+			Result result = new Result();
+			result.setElement("ReadStudent");
+			result.setRet_ftr(1);
+			result.setDet(2);
+			result.setComplexity("Low");
+			result.setComplexityValue(4);
+			results.add(result);
+		}
+
+		public List<Result> getResults() {
+			return results;
+		}
+
+	}
+
+	private void hookContextMenu()
+	{
 		MenuManager menuMgr = new MenuManager("#PopupMenu");
 		menuMgr.setRemoveAllWhenShown(true);
-		menuMgr.addMenuListener(new IMenuListener() {
-			public void menuAboutToShow(IMenuManager manager) {
+		menuMgr.addMenuListener(new IMenuListener()
+		{
+			public void menuAboutToShow(IMenuManager manager)
+			{
 				LigeiroView.this.fillContextMenu(manager);
 			}
 		});
@@ -114,77 +233,93 @@ public class LigeiroView extends ViewPart {
 		getSite().registerContextMenu(menuMgr, viewer);
 	}
 
-	private void contributeToActionBars() {
+	private void contributeToActionBars()
+	{
 		IActionBars bars = getViewSite().getActionBars();
 		fillLocalPullDown(bars.getMenuManager());
 		fillLocalToolBar(bars.getToolBarManager());
 	}
 
-	private void fillLocalPullDown(IMenuManager manager) {
+	private void fillLocalPullDown(IMenuManager manager)
+	{
 		manager.add(action1);
 		manager.add(new Separator());
 		manager.add(action2);
 	}
 
-	private void fillContextMenu(IMenuManager manager) {
+	private void fillContextMenu(IMenuManager manager)
+	{
 		manager.add(action1);
 		manager.add(action2);
 		// Other plug-ins can contribute there actions here
 		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 	}
-	
-	private void fillLocalToolBar(IToolBarManager manager) {
+
+	private void fillLocalToolBar(IToolBarManager manager)
+	{
 		manager.add(action1);
 		manager.add(action2);
 	}
 
-	private void makeActions() {
-		action1 = new Action() {
-			public void run() {
+	private void makeActions()
+	{
+		action1 = new Action()
+		{
+			public void run()
+			{
 				showMessage("Action 1 executed");
 			}
 		};
 		action1.setText("Action 1");
 		action1.setToolTipText("Action 1 tooltip");
-		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		
-		action2 = new Action() {
-			public void run() {
+		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+				.getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+
+		action2 = new Action()
+		{
+			public void run()
+			{
 				showMessage("Action 2 executed");
 			}
 		};
 		action2.setText("Action 2");
 		action2.setToolTipText("Action 2 tooltip");
-		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		doubleClickAction = new Action() {
-			public void run() {
+		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+				.getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		doubleClickAction = new Action()
+		{
+			public void run()
+			{
 				ISelection selection = viewer.getSelection();
-				Object obj = ((IStructuredSelection)selection).getFirstElement();
-				showMessage("Double-click detected on "+obj.toString());
+				Object obj = ((IStructuredSelection) selection)
+						.getFirstElement();
+				showMessage("Double-click detected on " + obj.toString());
 			}
 		};
 	}
 
-	private void hookDoubleClickAction() {
-		viewer.addDoubleClickListener(new IDoubleClickListener() {
-			public void doubleClick(DoubleClickEvent event) {
+	private void hookDoubleClickAction()
+	{
+		viewer.addDoubleClickListener(new IDoubleClickListener()
+		{
+			public void doubleClick(DoubleClickEvent event)
+			{
 				doubleClickAction.run();
 			}
 		});
 	}
-	private void showMessage(String message) {
-		MessageDialog.openInformation(
-			viewer.getControl().getShell(),
-			"Ligeiro View",
-			message);
+
+	private void showMessage(String message)
+	{
+		MessageDialog.openInformation(viewer.getControl().getShell(),
+				"Ligeiro View", message);
 	}
 
 	/**
-	 * Passing the focus request to the viewer's control.
+	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
 	 */
-	public void setFocus() {
+	public void setFocus()
+	{
 		viewer.getControl().setFocus();
 	}
 }
